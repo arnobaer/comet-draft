2fc1d38debf08e029073966f618690cae27fcad7
Bernhard Arnold <arnobaer@users.noreply.github.com>
2019-05-29 17:50:03 +0200

    Update test_devicemanager.py

--- tests/test_devicemanager.py
+++ tests/test_devicemanager.py
@@ -1,19 +1,25 @@
 import unittest
 import env
 
+from pyvisa import ResourceManager
+
 from comet import DeviceManager
 
 class DeviceManagerTestCase(unittest.TestCase):
     def setUp(self):
-        self.manager = DeviceManager()
+        rm = ResourceManager('@sim')
+        self.manager = DeviceManager(rm)
 
 class BasicDeviceManagerTestCase(DeviceManagerTestCase):
     def runTest(self):
         manager = self.manager
         self.assertEqual(manager.factory.__class__.__name__, 'DeviceFactory')
         self.assertEqual(type(manager.devices), dict)
-        manager.create('SMU1', {'name': 'keithley1234'})
-        device = manager.get('SMU1')
+        device = manager.create('SMU1', 'GPIB::16', {'get_value': {'method': 'query', 'message': 'VALUE?'})
+        self.assertTrue(device is manager.get('SMU1'))
+        self.assertEqual(device.name, 'SMU1')          
+        self.assertEqual(device.get_value.method, 'query')
+        self.assertEqual(device.get_value.message, 'VALUE?')
 
 if __name__ == '__main__':
     unittest.main()
