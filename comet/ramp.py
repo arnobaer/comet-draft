5b73b62966dc1537ff11ae348a28ca960171897a
Bernhard Arnold <bernhard.arnold@cern.ch>
2019-06-07 14:06:47 +0200

    added fixed precision ramping

--- comet/ramp.py
+++ comet/ramp.py
@@ -1,7 +1,7 @@
 import time
 
 class Ramp:
-    """Ramp iterator with integrated waiting.
+    """Fixed precision ramp iterator with integrated cycle delay.
 
     >>> ramp_up = Ramp(0, 10, step=.5, delay=0.25)
     >>> for v in ramp_up:
@@ -11,30 +11,55 @@ class Ramp:
     >>> for v in ramp_down:
     ...     device.set_voltage(v)
 
+    >>> ramp = Ramp(0, 1, step=-.1, prec=1) # set fixed point precision
+    >>> for v in ramp:
+    ...     print(v)
+
     """
 
-    def __init__(self, start, stop, step, delay=None):
-        self.start = start
-        self.stop = stop
-        self.step = step
-        self.delay = delay or 0.0
+    DEFAULT_PREC = 9
+
+    def __init__(self, start, stop, step, delay=None, prec=DEFAULT_PREC):
+        self.prec = prec
+        self.start = self._float_to_int(start)
+        self.stop = self._float_to_int(stop)
+        if self.start > self.stop:
+            self.step = -abs(self._float_to_int(step))
+        else:
+            self.step = abs(self._float_to_int(step))
+        if self.step == 0:
+            raise ValueError("step must not be zero")
         self.value = self.start
+        self.delay = float(delay)
 
     def wait(self):
-        time.sleep(self.delay)
+        if self.delay:
+            time.sleep(self.delay)
 
-    def running(self):
+    @property
+    def active(self):
+        if self.start == self.stop:
+            return False
         if self.start >= self.stop:
             return self.value >= self.stop
         return self.value <= self.stop
+
+    def _float_to_int(self, value):
+        return int(round(value * 10**self.prec))
+
+    def _int_to_float(self, value):
+        return value / 10**self.prec
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        self.wait()
-        if self.running():
-            value = self.value
+        # wait only between steps
+        start, stop = sorted((self.start, self.stop))
+        if start < self.value <= stop:
+            self.wait()
+        if self.active:
+            value = self._int_to_float(self.value)
             self.value = self.value + self.step
             return value
         raise StopIteration()
--- doc/ramping.md
+++ doc/ramping.md
@@ -1,6 +1,8 @@
 # Ramping
 
-A simple way to ramp up/down voltages or other values.
+A simple way to ramp up/down voltages or other values in fixed precision.
+
+Default precision is 9.
 
 ```python
 >>> from comet.ramp import Ramp
@@ -20,10 +22,10 @@ To ramp up define range, step and delay.
 10
 ```
 
-To ramp down reverse the range and step sign.
+To ramp down just reverse the range, the proper sign of the step is calculated.
 
 ```python
->>> ramp_down = Ramp(start=10.0, stop=0.0, step=-2.0, delay=0.25)
+>>> ramp_down = Ramp(start=10.0, stop=0.0, step=2.0, delay=0.25)
 >>> for v in ramp_down:
 ...     print(v)
 10
@@ -32,4 +34,18 @@ To ramp down reverse the range and step sign.
 4
 2
 0
+```
+
+To ramp using a certain precision use the `prec` argument. Values are rounded.
+
+```python
+>>> ramp = Ramp(start=0.0, stop=1.0, step=0.25, prec=1)
+>>> for v in ramp_down:
+...     print(v)
+0.0
+0.2
+0.4
+0.6
+0.8
+1.0
 ```
