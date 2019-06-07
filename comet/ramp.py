b5aa016d875c1b5e936e872813c7476113f60f59
Bernhard Arnold <bernhard.arnold@cern.ch>
2019-06-07 17:13:33 +0200

    improved ramp implementation using range

--- comet/ramp.py
+++ comet/ramp.py
@@ -11,55 +11,59 @@ class Ramp:
     >>> for v in ramp_down:
     ...     device.set_voltage(v)
 
-    >>> ramp = Ramp(0, 1, step=-.1, prec=1) # set fixed point precision
+    >>> ramp = Ramp(0, 1, step=.001, prec=3) # set fixed point precision
     >>> for v in ramp:
-    ...     print(v)
+    ...     device.set_voltage(v)
+
+    Every iteration is delayed with exception of first and last iteration.
+
+    Ramp(0,3,1): [0]<--delay-->[1]<--delay-->[2]<--delay-->[3]
 
     """
 
-    DEFAULT_PREC = 9
+    DEFAULT_PREC = 6
 
-    def __init__(self, start, stop, step, delay=None, prec=DEFAULT_PREC):
-        self.prec = prec
-        self.start = self._float_to_int(start)
-        self.stop = self._float_to_int(stop)
-        if self.start > self.stop:
-            self.step = -abs(self._float_to_int(step))
-        else:
-            self.step = abs(self._float_to_int(step))
-        if self.step == 0:
-            raise ValueError("step must not be zero")
-        self.value = self.start
-        self.delay = float(delay)
+    def __init__(self, start, stop, step, delay=0, prec=DEFAULT_PREC):
+        start = round(start * 10**prec)
+        stop = round(stop * 10**prec)
+        step = round(step * 10**prec)
+        stop += step
+        self.__range = range(start, stop, step)
+        self.__prec = prec
+        self.delay = delay
+        self.reset()
+
+    def reset(self):
+        """Reset range iterator."""
+        self.__iter = self.__range.__iter__()
 
     def wait(self):
-        if self.delay:
-            time.sleep(self.delay)
+        """Delay method, overwrite for custom implementation."""
+        time.sleep(self.delay)
 
     @property
-    def active(self):
-        if self.start == self.stop:
-            return False
-        if self.start >= self.stop:
-            return self.value >= self.stop
-        return self.value <= self.stop
+    def start(self):
+        return self.__range.start / 10**self.__prec
 
-    def _float_to_int(self, value):
-        return int(round(value * 10**self.prec))
+    @property
+    def stop(self):
+        return (self.__range.stop - self.__range.step) / 10**self.__prec
 
-    def _int_to_float(self, value):
-        return value / 10**self.prec
+    @property
+    def step(self):
+        return self.__range.step / 10**self.__prec
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        # wait only between steps
-        start, stop = sorted((self.start, self.stop))
-        if start < self.value <= stop:
-            self.wait()
-        if self.active:
-            value = self._int_to_float(self.value)
-            self.value = self.value + self.step
-            return value
-        raise StopIteration()
+        try:
+            value = next(self.__iter)
+            # Delay only iterations within range
+            start, stop = sorted((self.__range.start, self.__range.stop))
+            if start < value < stop:
+                self.wait()
+            return value / 10**self.__prec
+        except StopIteration:
+            self.reset()
+            raise
--- tests/test_ramp.py
+++ tests/test_ramp.py
@@ -23,13 +23,5 @@ class RampTest(unittest.TestCase):
             self.assertEqual(v, ref)
             ref += step
 
-    def testOverflow(self):
-        ref = 0
-        start = 0.
-        stop = 10.
-        step = 255
-        for v in Ramp(start, stop, step, delay=0.01):
-            self.assertEqual(v, ref)
-
 if __name__ == '__main__':
     unittest.main()
