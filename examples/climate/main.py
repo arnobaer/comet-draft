1a60f5d8bcfc31369a311b3da14610f8d3074f59
Bernhard Arnold <bernhard.arnold@cern.ch>
2019-06-17 12:34:57 +0200

    added simulation devices

--- examples/climate/main.py
+++ examples/climate/main.py
@@ -4,17 +4,23 @@ import comet
 class Application(comet.Application):
 
     def __init__(self):
-        super(Application, self).__init__('CLT')
+        super(Application, self).__init__('CLT', backend='@sim')
         # Set attributes
         self.set('default_op', 'Monty')
         # Register parameters
         self.register_param('v_max', default=1000.0, prec=1, unit='V')
         self.register_param('i_compliance', default=2.5, unit='A')
         self.register_param('operator', default=self.get('default_op'), type=str)
+        # register devices
+        self.register_device('climate', 'ASRL1::INSTR')
+        self.register_device('smu', 'ASRL2::INSTR')
+        self.register_device('multi', 'ASRL3::INSTR')
+        self.register_device('shunt', 'ASRL4::INSTR')
         # Register collections
         self.register_collection('environ', EnvironCollection)
         self.register_collection('iv', IVCollection)
         # Register procedures
+        self.register_procedure('monitoring', Monitoring, continious=True)
         self.register_procedure('ramp_up', RampUp)
         self.register_procedure('ramp_down', RampDown)
 
