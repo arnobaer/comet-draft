ee5e87b329cefd8b9a04982844d8420d52ca8357
Bernhard Arnold <bernhard.arnold@cern.ch>
2019-06-18 19:26:20 +0200

    added progress attr

--- comet/procedure.py
+++ comet/procedure.py
@@ -6,6 +6,8 @@ class Procedure:
         super(Procedure, self).__init__()
         self.__app = app
         self.__name = name
+        self.__label = None
+        self.__progress = 0.0
 
     @property
     def app(self):
@@ -14,6 +16,20 @@ class Procedure:
     @property
     def name(self):
         return self.__name
+
+    @property
+    def label(self):
+        return self.__name.replace('_', ' ').capitalize()
+
+    @property
+    def progress(self):
+        """Returns procedure progress in percent."""
+        return self.__progress
+
+    @progress.setter
+    def progress(self, percent):
+        """Set procedure progress in percent."""
+        self.__progress = max(0.0, min(100.0, float(percent)))
 
     def wait(self, delay):
         """Halt and wait for delay."""
