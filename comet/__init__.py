b78a441db65d8dabf2c7c87daee0ccb7bf388e0f
Bernhard Arnold <bernhard.arnold@cern.ch>
2019-06-19 12:28:30 +0200

    removed module commandline

--- comet/__init__.py
+++ comet/__init__.py
@@ -1,9 +1,8 @@
 __version__ = '1.0.0'
 
 from .application import Application, Parameter
-from .commandline import CommandLine
 from .httpserver import HttpServer
 from .collection import Collection
-from .procedure import Procedure
+from .state import State
 from .device import Device
 from .filewriter import FileWriter, CSVFileWriter, HephyDBFileWriter
